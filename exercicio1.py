bb333e55b197492eac1294619ca7b13ef57bb631
Denis Fernando Gentil <74164449+gentildf@users.noreply.github.com>
2021-03-16 05:22:17 -0300

    organizando pasta
    organizando pasta

--- exercicio1.py
+++
@@ -1,4 +0,0 @@
-nome = input ('Digite seu nome: ')
-idade = input ('Digite sua idade: ')
-peso =  input ('Digite seu peso: ')
-print('Seu nome é:' , nome , 'Sua idade é:' , idade , 'Seu peso é: ' , peso)
